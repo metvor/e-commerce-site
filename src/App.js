31d5f1aebe1c5c890775bcefde9057d6e5321666
Mette Vorraa <mettevorraa@gmail.com>
2022-03-16 21:52:36 +0000

    Import Navbar from bootstrap + replace ul menu with Navbar component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import ShoppingCart from './ShoppingCart.js';
 import ProductImages from './ProductImages.js';
 import ProductInfo from './ProductInfo.js';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App() {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>sneakers</li>
-          <li>Collections</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li><ShoppingCart/></li>
-        </ul>
-      </nav>
+      <Navbar bg="light" expand="lg">
+      <Nav className="brand">sneakers</Nav>
+      <Nav>Collections</Nav>
+      <Nav>Men</Nav>
+      <Nav>Women</Nav>
+      <Nav>About</Nav>
+      <Nav>Contact</Nav>
+      <Nav><ShoppingCart/></Nav>
+      </Navbar>
       <div className='row'>
         <div className='col-6'><ProductImages/></div>
         <div className='col-6'><ProductInfo/></div>
