9ce8cf2b166d0f96f8146217773befd1ba9ec1b2
Mette Vorraa <mettevorraa@gmail.com>
2022-03-21 21:16:10 +0000

    Improve navbar

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import ShoppingCart from './ShoppingCart.js';
 import ProductImages from './ProductImages.js';
 import ProductInfo from './ProductInfo.js';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import avatar from './images/image-avatar.png';
 
 
 export default function App() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-      <Nav className="brand">sneakers</Nav>
-      <Nav>Collections</Nav>
-      <Nav>Men</Nav>
-      <Nav>Women</Nav>
-      <Nav>About</Nav>
-      <Nav>Contact</Nav>
-      <Nav><ShoppingCart/></Nav>
+      <Navbar bg="light" expand="lg" className="navbar">
+      <nav className='nav brand'>sneakers</nav>
+      <nav className='nav'>Collections</nav>
+      <nav className='nav'>Men</nav>
+      <nav className='nav'>Women</nav>
+      <nav className='nav'>About</nav>
+      <nav className='nav'>Contact</nav>
+      <nav className='nav cart'><ShoppingCart/></nav>
+      <nav className='nav avatar'><img src={avatar} alt="avatar" max-width={10}/></nav>
       </Navbar>
       <div className='row'>
         <div className='col-6'><ProductImages/></div>
         <div className='col-6'><ProductInfo/></div>
       </div>
+      
       <p>Coded by MV. Challenge by Frontend Mentor.</p>
     </div>
   )
